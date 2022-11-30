448f273b6b1b58253114134cbb2075c44d2b9814
mmvergara <mark.jesusmanabat@gmail.com>
2022-12-01 06:57:56 +0800

    implemented simple auth process

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,12 +1,18 @@
 import { Button, TextInput } from "@mantine/core";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
+import { userEmail } from "../Config";
+import { authFB } from "../firebase";
 
 const Login: React.FC = () => {
-  const [pass, setPass] = useState<string>();
-
+  const [pass, setPass] = useState<string>("");
+  const loginHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = signInWithEmailAndPassword(authFB, userEmail, pass);
+    console.log(data);
+  };
   return (
-    <>
-      {" "}
+    <form onSubmit={loginHandler}>
       <TextInput
         placeholder='Password'
         type='password'
@@ -14,7 +20,7 @@ const Login: React.FC = () => {
         value={pass}
       />
       <Button>Login</Button>
-    </>
+    </form>
   );
 };
 
